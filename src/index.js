83e5e299d72224c22f9d93fa8c9612ea0f1a6045
nguyenhuutuong999 <nguyenhuutuong999@gmail.com>
2021-04-19 17:46:12 +0700

    udpate

--- src/index.js
+++ src/index.js
@@ -8,21 +8,39 @@ const port = 3000;
 const app = express();
 
 app.use(morgan('combined'))
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json())
+
 app.engine('hbs', handlebars({
   extname: '.hbs'
 }))
 app.set("view engine", 'hbs')
 app.set('views', path.join(__dirname,'resources\\views'))
+
 // const server = http.createServer((req, res) => {
 //   res.statusCode = 200;
 //   res.setHeader('Content-Type', 'text/plain');
 //   res.end('Hello, World!\n');
 // });
-
 app.get('/', (req, res) => {
+  return res.render('home')
+});
+app.get('/news', (req, res) => {
+  console.log("new", req.query)
   return res.render('news')
 });
 
+app.get('/search', (req, res) => {
+  console.log(req.query)
+  return res.render('search')
+});
+
+app.post('/search', (req, res) => {
+  console.log(req.body)
+  return res.render('search')
+});
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
