e42ecccb0ea181af385f9fee6fa8c670e67814ed
nguyenhuutuong999 <nguyenhuutuong999@gmail.com>
2021-04-20 16:11:57 +0700

    Config Database

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ const handlebars = require('express-handlebars');
 const hostname = '127.0.0.1';
 const port = 3000;
 const app = express();
+const route = require("./routes");
+
+// Connect to DB
+const db = require('./config/db')
+db.connect()
 
 app.use(morgan('combined'))
 app.use(express.urlencoded({
@@ -17,30 +22,17 @@ app.engine('hbs', handlebars({
   extname: '.hbs'
 }))
 app.set("view engine", 'hbs')
-app.set('views', path.join(__dirname,'resources\\views'))
+app.set('views', path.join(__dirname, 'resources', 'views'))
+
+//routes inits
+route(app);
 
 // const server = http.createServer((req, res) => {
 //   res.statusCode = 200;
 //   res.setHeader('Content-Type', 'text/plain');
 //   res.end('Hello, World!\n');
 // });
-app.get('/', (req, res) => {
-  return res.render('home')
-});
-app.get('/news', (req, res) => {
-  console.log("new", req.query)
-  return res.render('news')
-});
 
-app.get('/search', (req, res) => {
-  console.log(req.query)
-  return res.render('search')
-});
-
-app.post('/search', (req, res) => {
-  console.log(req.body)
-  return res.render('search')
-});
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
