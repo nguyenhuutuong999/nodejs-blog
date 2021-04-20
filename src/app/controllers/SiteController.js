33fcbfd16bdee821bb8f75ad3f9e125963c36e7f
nguyenhuutuong999 <nguyenhuutuong999@gmail.com>
2021-04-20 17:01:57 +0700

    add helper mongooses

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,18 +1,15 @@
-const Course = require('../models/Course');
-
+const Course = require("../models/Course");
+const { multipleMongoosesToObject } = require("../../util/mongoose");
 class SiteController {
-    index(req, res){
-        // res.render('home')
-       Course.find({}, function(err, courses){
-           if(!err) {res.json(courses);}
-           else{
-            res.status(400).json({error: "ERROR !!!"})
-           }
-          
-       })
-    }
-    search(req, res){
-        res.render('search')
-    }
+  index(req, res, next) {
+    Course.find({})
+      .then((courses) => {
+        res.render("home", { courses: multipleMongoosesToObject(courses) });
+      })
+      .catch(next);
+  }
+  search(req, res) {
+    res.render("search");
+  }
 }
-module.exports = new SiteController;
+module.exports = new SiteController();
